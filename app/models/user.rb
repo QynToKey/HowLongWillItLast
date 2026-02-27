190617173d802f8d9373b9c7d42e89acfc10b5c8
QynToKey <qyntokey@icloud.com>
2026-02-27 15:58:46 +0900

    add: Sorceryの認証設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  before_validation { email&.downcase! } # 保存時に正規化
+  
+  validates :email,
+            presence: true,
+            uniqueness: true
+  validates :password,
+            presence:true,
+            confirmation: true,
+            length: { minimum: 6 },
+            if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation,
+            presence: true,
+            if: -> { new_record? || changes[:crypted_password] }
 end
