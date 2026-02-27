14779582ac5c0ec2eb52bee53464b1e9bc582717
QynToKey <qyntokey@icloud.com>
2026-02-27 16:42:59 +0900

    update: user.rb のメール一意制約を防御的に強化

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   before_validation { email&.downcase! } # 保存時に正規化
-  
+
   validates :email,
             presence: true,
-            uniqueness: true
+            uniqueness: { case_sensitive: false } # Email の重複判定で大文字小文字を区別しない
   validates :password,
             presence:true,
             confirmation: true,
