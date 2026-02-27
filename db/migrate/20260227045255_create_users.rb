82c527c5db35d583c5589438c63c65209beb8057
QynToKey <qyntokey@icloud.com>
2026-02-27 14:07:00 +0900

    add: create users table

--- db/migrate/20260227045255_create_users.rb
+++ db/migrate/20260227045255_create_users.rb
@@ -1,7 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :email, null: false
+      t.string :crypted_password, null: false
+      t.string :salt, null: false
+
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end
