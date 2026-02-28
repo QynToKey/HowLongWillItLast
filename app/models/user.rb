e463edd4e40ed79e5db6917cac4eee86f2fb7db3
QynToKey <qyntokey@icloud.com>
2026-02-28 17:51:57 +0900

    add: 登録フォームに「名前」を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,13 @@ class User < ApplicationRecord
 
   before_validation { email&.downcase! } # 保存時に正規化
 
-  validates :email,
-            presence: true,
+  validates :name, presence: true
+  validates :email, presence: true,
             uniqueness: { case_sensitive: false } # Email の重複判定で大文字小文字を区別しない
-  validates :password,
-            presence:true,
+  validates :password, presence:true,
             confirmation: true,
             length: { minimum: 6 },
             if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation,
-            presence: true,
+  validates :password_confirmation, presence: true,
             if: -> { new_record? || changes[:crypted_password] }
 end
