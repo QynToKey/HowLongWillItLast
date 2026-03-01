31ee04045cedcf2866a41490df9a5b1ec865253a
QynToKey <qyntokey@icloud.com>
2026-03-01 14:52:51 +0900

    add : login_path をルーティングに追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get "home/index"
   root "home#index"
 
+  get 'login',  to: 'user_sessions#new'
+  post 'login',  to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
   resources :users, only: %i[new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
