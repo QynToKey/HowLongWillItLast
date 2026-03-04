f000091e0fcdfd94d1b25939165683ddeb6c911e
QynToKey <qyntokey@icloud.com>
2026-03-04 17:34:14 +0900

    add: User モデルにアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   before_validation { email&.downcase! } # 保存時に正規化
 
+  has_many :learning_themes, dependent: :destroy
+
   validates :name, presence: true
   validates :email, presence: true,
             uniqueness: { case_sensitive: false } # Email の重複判定で大文字小文字を区別しない
