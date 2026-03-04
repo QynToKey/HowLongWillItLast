f50a86cd48f8cf1c510b7673c58f1f420423d448
QynToKey <qyntokey@icloud.com>
2026-03-04 17:16:54 +0900

    add: migration に制約を追記

--- db/migrate/20260304080053_create_learning_themes.rb
+++ db/migrate/20260304080053_create_learning_themes.rb
@@ -1,11 +1,13 @@
 class CreateLearningThemes < ActiveRecord::Migration[7.2]
   def change
     create_table :learning_themes do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :description
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true { on_delete: :cascade }
 
       t.timestamps
     end
+
+    add_index :learning_themes, [:user_id, :name], unique: true
   end
 end
