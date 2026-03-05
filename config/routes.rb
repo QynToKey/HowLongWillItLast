d3fdb7d176dff7d75d7317baa459ae155492ab94
QynToKey <qyntokey@icloud.com>
2026-03-05 12:19:09 +0900

    add: ルーティングに learning_themes を追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   delete "logout", to: "user_sessions#destroy"
 
   resources :users, only: %i[new create]
+  resources :learning_themes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
