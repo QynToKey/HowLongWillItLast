ac401f1587c492553ce60c51a610778017e2d0fb
QynToKey <qyntokey@icloud.com>
2026-03-05 17:26:57 +0900

    fix: route.rb の重複を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get "learning_themes/new"
-  get "learning_themes/create"
-  get "learning_themes/index"
-  get "learning_themes/edit"
-  get "learning_themes/update"
-  get "learning_themes/destroy"
-  get "home/index"
   root "home#index"
 
   get "login",  to: "user_sessions#new"
@@ -13,7 +6,7 @@ Rails.application.routes.draw do
   delete "logout", to: "user_sessions#destroy"
 
   resources :users, only: %i[new create]
-  resources :learning_themes
+  resources :learning_themes, except: %i[show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
