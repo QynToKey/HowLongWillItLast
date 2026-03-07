eaff5983f2de6260886c95452f2cc24b721a1375
QynToKey <qyntokey@icloud.com>
2026-03-07 16:26:16 +0900

    add: Tag モデルにアソシエーションを追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,6 @@ class Tag < ApplicationRecord
 
   has_many :record_tags, dependent: :destroy
   has_many :learning_records, through: :record_tags
+  has_many :todo_tags, dependent: :destroy
+  has_many :todos, through: :todo_tags
 end
