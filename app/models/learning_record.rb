db72b44e42fda8ed255b7ef1b3d8660e17f68534
QynToKey <qyntokey@icloud.com>
2026-03-08 13:55:02 +0900

    add: LearningRecore モデルにバリデーションを追加

--- app/models/learning_record.rb
+++ app/models/learning_record.rb
@@ -3,4 +3,10 @@ class LearningRecord < ApplicationRecord
 
   has_many :record_tags, dependent: :destroy
   has_many :tags, through: :record_tags
+
+  validates :study_date, presence: true
+  validates :content, presence: true
+  validates :duration_minutes, allow_nil: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :started_at, allow_nil: true
+  validates :ended_at, allow_nil: true
 end
