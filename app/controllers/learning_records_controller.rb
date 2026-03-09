0360156c43ae29e81ed46bea088a61b288cac808
QynToKey <qyntokey@icloud.com>
2026-03-09 15:57:16 +0900

    add: edit / update アクションを追加

--- app/controllers/learning_records_controller.rb
+++ app/controllers/learning_records_controller.rb
@@ -11,6 +11,7 @@ class LearningRecordsController < ApplicationController
   end
 
   def create
+    # ユーザーが所有する学習記録を作成する
     @learning_record = current_user.learning_records.build(learning_record_params)
 
     if @learning_record.save
@@ -22,7 +23,24 @@ class LearningRecordsController < ApplicationController
   end
 
   def show
+    # ユーザーが所有する学習記録のみを表示できるようにする
     @learning_record = current_user.learning_records.find(params[:id])
+  end
+
+  def edit
+    # ユーザーが所有する学習記録のみを編集できるようにする
+    @learning_record = current_user.learning_records.find(params[:id])
+  end
+
+  def update
+    @learning_record = current_user.learning_records.find(params[:id])
+
+    if @learning_record.update(learning_record_params)
+      redirect_to @learning_record, notice: "学習記録を更新しました"
+    else
+      flash.now[:alert] = "学習記録の更新に失敗しました"
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
