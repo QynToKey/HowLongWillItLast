e70a083e28d0ce2f4cedd5f687edef7769061851
QynToKey <qyntokey@icloud.com>
2026-03-11 17:51:52 +0900

    add: users の edit / update / destroy をルーティングに追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post "login",  to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
 
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update destroy]
   resources :learning_records
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
