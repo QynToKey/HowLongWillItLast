ea0549c4accac3d657ed6030d23a8abf488981c3
QynToKey <qyntokey@icloud.com>
2026-03-16 13:30:09 +0900

    add: stopwatch ビューから DOM 要素を取得する処理を追加

--- app/javascript/controllers/stopwatch_controller.js
+++ app/javascript/controllers/stopwatch_controller.js
@@ -1,7 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  // DOM要素を取得するためのターゲットを定義
+  static targets = ["timer", "start", "stop", "reset"]
+
+  // ストップウォッチの状態を管理するためのプロパティを定義
+  startTime = undefined
+  timeoutId = undefined
+  elapsedTime = 0
+
   connect() {
-    console.log("stopwatch connected!")
+    console.log(this.timerTarget)
+    console.log(this.startTarget)
+    console.log(this.stopTarget)
+    console.log(this.resetTarget)
   }
 }
