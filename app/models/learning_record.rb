088de74a13fda83b5635a79fe1401eaa26803411
QynToKey <qyntokey@icloud.com>
2026-03-16 17:50:05 +0900

    fix: LearningRecord モデルのコールバックを削除

--- app/models/learning_record.rb
+++ app/models/learning_record.rb
@@ -1,9 +1,6 @@
 class LearningRecord < ApplicationRecord
   belongs_to :user
 
-  # バリデーションの前に学習時間を計算するコールバック
-  before_validation :calculate_duration
-
   has_many :record_tags, dependent: :destroy
   has_many :tags, through: :record_tags
 
