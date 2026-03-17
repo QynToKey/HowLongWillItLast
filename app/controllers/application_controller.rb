63c542022ad33162a3a2da785beec2626f4956f5
QynToKey <qyntokey@icloud.com>
2026-03-17 13:28:30 +0900

    add: 未ログインユーザーを login_path へリダイレクトさせるための明示的な処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in? # ログイン状態をビューで確認できるようにする
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def not_authenticated
+    redirect_to login_path, alert: "ログインしてください"
+  end
 end
