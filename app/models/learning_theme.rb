9b9807033701ceca6da0cf2e5a1e9edda4723cd8
QynToKey <qyntokey@icloud.com>
2026-03-28 17:27:11 +0900

    add: LearningTheme モデルにバリデーションを追加

--- app/models/learning_theme.rb
+++ app/models/learning_theme.rb
@@ -1,3 +1,6 @@
 class LearningTheme < ApplicationRecord
   belongs_to :user
+
+  # 自分自身を除いた他の learning_theme の存在を確認し、存在すれば２つ目以降の name を必須にする
+  validates :name, presence: true, if: -> { user.learning_themes.where.not(id: id).exists? }
 end
