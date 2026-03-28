390e819217dd22c4d6142988f9909ba231296f21
QynToKey <qyntokey@icloud.com>
2026-03-28 14:30:04 +0900

    add: LearningTheme モデルを生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_21_093259) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_28_045747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_21_093259) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_learning_records_on_user_id"
+  end
+
+  create_table "learning_themes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id", "name"], name: "index_learning_themes_on_user_id_and_name", unique: true, where: "(name IS NOT NULL)"
+    t.index ["user_id"], name: "index_learning_themes_on_user_id"
   end
 
   create_table "record_tags", force: :cascade do |t|
@@ -75,6 +84,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_21_093259) do
   end
 
   add_foreign_key "learning_records", "users"
+  add_foreign_key "learning_themes", "users", on_delete: :cascade
   add_foreign_key "record_tags", "learning_records"
   add_foreign_key "record_tags", "tags"
   add_foreign_key "tags", "users"
