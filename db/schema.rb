a5bf4869196929d80f9a3a93b77010f8c0f5c750
QynToKey <qyntokey@icloud.com>
2026-03-28 15:26:55 +0900

    feat: 既存の LearningTheme データを移行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_28_045747) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_28_060103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -76,7 +76,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_28_045747) do
     t.datetime "created_at", null: false
     t.string "crypted_password", null: false
     t.string "email", null: false
-    t.string "learning_theme"
     t.string "name"
     t.string "salt", null: false
     t.datetime "updated_at", null: false
