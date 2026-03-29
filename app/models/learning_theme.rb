21b0b2d470eae58815ee34d00d0e6d467a1492b4
QynToKey <qyntokey@icloud.com>
2026-03-29 17:08:55 +0900

    add: モデル側にアソシエーションを設定

--- app/models/learning_theme.rb
+++ app/models/learning_theme.rb
@@ -3,4 +3,7 @@ class LearningTheme < ApplicationRecord
 
   # 自分自身を除いた他の learning_theme の存在を確認し、存在すれば２つ目以降の name を必須にする
   validates :name, presence: true, if: -> { user.learning_themes.where.not(id: id).exists? }
+
+  has_many :learning_records, dependent: :destroy
+  has_many :tags, dependent: :destroy
 end
