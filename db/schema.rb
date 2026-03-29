eca7719dfeec15f56ca75ddd91d705ab62a79c98
QynToKey <qyntokey@icloud.com>
2026-03-29 11:58:08 +0900

    feat: remember_me をマイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_28_060103) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_29_024027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -77,6 +77,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_28_060103) do
     t.string "crypted_password", null: false
     t.string "email", null: false
     t.string "name"
+    t.string "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
     t.string "salt", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
