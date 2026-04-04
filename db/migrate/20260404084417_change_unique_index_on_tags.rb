435a6fe8c36a72342ec13e33cb926cc853ac5c9c
QynToKey <qyntokey@icloud.com>
2026-04-04 18:20:28 +0900

    fix: Lint エラーに対応

--- db/migrate/20260404084417_change_unique_index_on_tags.rb
+++ db/migrate/20260404084417_change_unique_index_on_tags.rb
@@ -1,6 +1,6 @@
 class ChangeUniqueIndexOnTags < ActiveRecord::Migration[8.1]
   def change
-    remove_index :tags, [:user_id, :name]
-    add_index :tags, [:learning_theme_id, :name], unique: true
+    remove_index :tags, [ :user_id, :name ]
+    add_index :tags, [ :learning_theme_id, :name ], unique: true
   end
 end
