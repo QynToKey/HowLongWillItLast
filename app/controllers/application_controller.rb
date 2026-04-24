914a89d2d748a12e5102eddd502d702d5ece10a1
QynToKey <qyntokey@icloud.com>
2026-04-24 10:13:20 +0900

    add: ApplicationController に Pagy::Backend を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
   before_action :require_login
   skip_before_action :require_login, only: [ :about, :logs ]
   helper_method :logged_in? # ログイン状態をビューで確認できるようにする
